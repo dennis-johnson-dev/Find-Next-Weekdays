716f575039c78e3f8ccfdd1144456054aacc5b04
Dennis Johnson <flapjacks999@gmail.com>
2012-12-16 21:02:07 -0700

    added selection of Electronic or Paper routing

--- date_form.php
+++ date_form.php
@@ -3,20 +3,38 @@
 /*
 2012 - Dennis Johnson
 
-This script allows you to get the date of 14th workday. This excludes weekends and holidays. 
+This script allows you to get the date of the nth business day. This excludes weekends and holidays. 
 
 You add your holidays of the year to the $holidays array and the script converts them to Unix timestamps from 1970.
 */
 
 if(isset($_POST['input_date']) && isset($_POST['submit']))
   {
+    // $total is the number of business days the script interprets
+    $total = 0;
+    $error = '';
+   
+    // checks the values of the radio inputs selecting either paper or electronic 
+    if(isset($_POST['submission_type']) && $_POST['submission_type'] == "electronic")
+    {
+      $total = 12;
+    }
+    else if(isset($_POST['submission_type']) && $_POST['submission_type'] == "paper")
+    {
+      $total = 5;
+    }
+    else 
+    {
+      $error = "Please select Electronic or Paper";
+    }
 
     // set default timezone so PHP knows where to base the times off of
 
     date_default_timezone_set('America/Denver');
 
+    // human readable holidays YEAR-MO-DA
     $holidays = array(
-    "2012-10-18", "2012-10-19", "2012-9-4", "2012-11-23", "2012-11-22", "2012-12-25", "2012-12-31"
+    "2012-10-18", "2012-10-19", "2012-9-4", "2012-11-23", "2012-11-22", "2012-12-25", "2012-12-24", "2013-01-01", "2013-01-21", "2013-02-18", "2013-05-27", "2013-07-04", "2013-09-02", "2013-10-14", "2013-11-11", "2013-11-18", "2013-11-29", "2013-12-25", "2013-12-24" 
     );
 
     $converted_holidays = array ();
@@ -29,12 +47,14 @@ if(isset($_POST['input_date']) && isset($_POST['submit']))
         $converted_holidays[] = $unix_holidays_item;
     }
 
+    // as the script finds valid weekdays, it puts the UNIX timestamp in the weekdays array
     $weekdays = array ();
 
-    // receives the date input from form
+    // receives the date input from form and checks for a valid date
+    // if there is an exception and PHP computes an invalid date
+    // it returns an error to the user
     date_default_timezone_set('America/Denver');
     $date = '';
-    $error = '';
     try {
         $date = new DateTime($_POST['input_date']);
     } catch (Exception $e) {
@@ -61,8 +81,8 @@ if(isset($_POST['input_date']) && isset($_POST['submit']))
               $workdays += 1; 
           }
  
-          // once 14 workdays are found, the script exits the loop 
-          if($workdays == 14)
+          // once the desired number of workdays are found, the script exits the loop 
+          if($workdays == $total)
           {
               $final_time = $date_format-($i*86400);
               break;
@@ -76,11 +96,12 @@ if(isset($_POST['input_date']) && isset($_POST['submit']))
 ?>
 
 <!DOCTYPE>
-<html>
+<html lang="en">
 <head>
+<meta charset=utf-8>
 <title>Date Helper</title>
 <link rel="stylesheet" type="text/css" href="style.css" />
-<script type="text/javascript" src="./jquery-1.8.2.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
 <script type="text/javascript">
 $(document).ready(function(){
   $('#submit').click(function(event) {
@@ -104,24 +125,29 @@ function validate() {
       $(".error_message").text('*The date you entered is either empty or is not formatted properly.');
       return false;
     }
-  
 }
 </script>
 </head>
 <body>
 
 <h1>Routing Date Helper</h1>
-
 <form id="date_form" name="date_form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
 <p>
 Please enter the desired date in the format of <strong>01/11/12</strong><br /> where 01 is the month (January), 11 is the day, and 12 is the year.</p>
+<p>Please select the format of your submission.</p>
+
+<p>
+<input type="radio" name="submission_type" value="electronic"> Electronic (12 days)
+<input type="radio" name="submission_type" value="paper"> Paper (5 days)
+</p>
+
 <p>
 <label for="input_date">Input Date: </label>
 <input id="input_date" type="textarea" name="input_date" />
-
 <input id="submit" type="submit" name="submit" value="submit" />
 <input type="submit" name="clear" value="clear" />
 </form>
+</p>
 
 <p class="error_message"></p>
 <?php
@@ -143,7 +169,7 @@ if(isset($time))
 ?>
 
 
-<ul>
+<ol>
 <?php
  
   if(isset($weekdays))
@@ -155,7 +181,7 @@ if(isset($time))
   }
 
 ?>
-</ul>
+</ol>
 
 </body>
 </html>
